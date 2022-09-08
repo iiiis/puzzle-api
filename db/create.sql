c6a4b8e54bf0a104823c59d757c57b1386d3b6b2
John Daglish <j.daglish@claritycreation.com>
2022-09-08 22:39:44 +0100

    UPDATE db schema

--- db/create.sql
+++ db/create.sql
@@ -1,6 +1,15 @@
-CREATE TABLE `puzzles` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(255) NOT NULL,
-    `added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `puzzles` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `type` VARCHAR(255) NOT NULL DEFAULT 'sudoku',
+    `added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `cells` (
+    `index` INT NOT NULL,
+    `puzzle_id` INT NOT NULL,
+    `value` VARCHAR(3),
+    `is_fixed` BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (`index`, `puzzle_id`),
+    FOREIGN KEY (`puzzle_id`) REFERENCES `puzzles` (`id`) ON DELETE CASCADE
 );
